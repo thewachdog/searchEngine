0b2dc8b0b01f9df201dc3cdb9efb258d59d118b5
Parrot Live user <user@localhost.localdomain>
2021-12-14 12:36:08 +0000

    crawler.py update

---
+++ crawler.py
@@ -0,0 +1,32 @@
+import socket
+import nmap
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+myIP = s.getsockname()[0]
+# print(myIP)
+s.close()
+
+nm = nmap.PortScanner()
+
+file1 = open('upHosts.txt', 'r')
+lines = file1.readlines()
+
+for ip in lines:
+    print(ip)
+    # nm.scan(myIP, "22-443")
+    # print(nm.scaninfo())
+    for i in range(22,444):
+    # scan the target port
+        res = nm.scan(ip,str(i))
+    # the result is a dictionary containing
+    # several information we only need to
+    # check if the port is opened or closed
+    # so we will access only that information
+    # in the dictionary
+        # print(res['state'])
+        res = res['scan'][ip.strip('\n')]['tcp'][i]['state']
+        # print(res['state'])
+        # print(f"{res}")
+        if f"{res}" == "open":
+            print(f'port {i} is {res}.')
